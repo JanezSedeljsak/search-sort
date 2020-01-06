a0adfec7fabd4154108a783e65d3e70d7a9692f0
JanezTheProgramer <43420276+JanezTheProgramer@users.noreply.github.com>
2020-01-06 23:30:25 +0100

    performance calculations

--- app/components/js/home.js
+++ app/components/js/home.js
@@ -51,50 +51,68 @@ document.addEventListener("DOMContentLoaded", function () {
                 Methods.fAlert(Methods.gAvg(this.pArray, Number(this.index)));
             },
             doIncludes() {
+                let t0 = performance.now();
                 Methods.fAlert(
                     Methods.gIncludes(this.pArray, Number(this.index), Number(this.iNum))
                 );
+                let t1 = performance.now();
+                console.log("Call to default Search took " + (t1 - t0) + " milliseconds.");
             },
             doIncludesB() {
+                let t0 = performance.now();
                 Methods.fAlert(
                     Methods.gIncludesB(this.pArray, Number(this.index), Number(this.iNum))
                 );
+                let t1 = performance.now();
+                console.log("Call to Binary Search took " + (t1 - t0) + " milliseconds.");
             },
 
             // selective sort
             async doSortS() {
+                let t0 = performance.now();
                 let response = await Methods.gSortS(this.pArray, Number(this.index));
                 Methods.fAlert(response.slice(0, Number(this.index)));
                 this.$nextTick(function () {
                     this.$set(this.pArray, response);
                 });
+                let t1 = performance.now();
+                console.log("Call to Selective Sort took " + (t1 - t0) + " milliseconds.");
             },
 
             // bubble sort
             async doSortB() {
+                let t0 = performance.now();
                 let response = await Methods.gSortB(this.pArray, Number(this.index));
                 Methods.fAlert(response.slice(0, Number(this.index)));
                 this.$nextTick(function () {
                     this.$set(this.pArray, response);
                 });
+                let t1 = performance.now();
+                console.log("Call to Bubble Sort took " + (t1 - t0) + " milliseconds.");
             },
 
             // quick sort
             async doSortQ() {
+                let t0 = performance.now();
                 let response = await Methods.gSortQ(this.pArray, Number(this.index));
                 Methods.fAlert(response.slice(0, Number(this.index)));
                 this.$nextTick(function () {
                     this.$set(this.pArray, response);
                 });
+                let t1 = performance.now();
+                console.log("Call to Quick Sort took " + (t1 - t0) + " milliseconds.");
             },
 
             // insertion sort
             async doSortI() {
+                let t0 = performance.now();
                 let response = await Methods.gSortI(this.pArray, Number(this.index));
                 Methods.fAlert(response.slice(0, Number(this.index)));
                 this.$nextTick(function () {
                     this.$set(this.pArray, response);
                 });
+                let t1 = performance.now();
+                console.log("Call to Insertion Sort took " + (t1 - t0) + " milliseconds.");
             }
         }
     });
