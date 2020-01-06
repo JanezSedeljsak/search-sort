30cf85e56e74a447fdfcbbe85e515cabb0bf360b
JanezTheProgramer <43420276+JanezTheProgramer@users.noreply.github.com>
2020-01-06 21:25:17 +0100

    custom methods implemented

--- app/components/js/home.js
+++ app/components/js/home.js
@@ -1,61 +1,65 @@
-document.addEventListener("DOMContentLoaded", function() {
-  new Vue({
-    el: "#root",
-    data: () => {
-      return {
-        iNum: "",
-        index: 0,
-        pArray: new Array(100).fill(null)
-      };
-    },
-    filters: {
-      // embeded js methods only for display function
-      stringify: function(value) {
-        let index = value.indexOf(null);
-        let dArray = value.slice(0, index).join(", ");
-        return dArray ? dArray : "Empty Array";
-      }
-    },
-    methods: {
-      doAdd() {
-        if (Number.isFinite(Number(this.iNum)) && this.iNum != "") {
-          this.pArray[this.index] = Number(this.iNum);
-          this.index++;
-          this.iNum = "";
-        } else {
-          Methods.fAlert("Input doesn't contain a nubmer!");
+document.addEventListener("DOMContentLoaded", function () {
+    new Vue({
+        el: "#root",
+        data: () => {
+            return {
+                iNum: "",
+                index: 0,
+                pArray: new Array(100).fill(null)
+            };
+        },
+        filters: {
+            // embeded js methods only for display function
+            stringify: function (value) {
+                let index = value.indexOf(null);
+                let dArray = value.slice(0, index).join(", ");
+                return dArray ? dArray : "Empty Array";
+            }
+        },
+        methods: {
+            doAdd() {
+                if (Number.isFinite(Number(this.iNum))) {
+                    this.pArray[this.index] = Math.round(Number(this.iNum) * 100) / 100;
+                    this.index++;
+                    this.iNum = "";
+                } else {
+                    Methods.fAlert("Input is not a valid nubmer!");
+                }
+            },
+            emptyArray() {
+                this.index = 0;
+                this.pArray = new Array(100).fill(null);
+            },
+            doMin() {
+                Methods.fAlert(Methods.gMin(this.pArray, Number(this.index)));
+            },
+            doMax() {
+                Methods.fAlert(Methods.gMax(this.pArray, Number(this.index)));
+            },
+            doSum() {
+                Methods.fAlert(Methods.gSum(this.pArray, Number(this.index)));
+            },
+            doAvg() {
+                Methods.fAlert(Methods.gAvg(this.pArray, Number(this.index)));
+            },
+            doIncludes() {
+                Methods.fAlert(Methods.gIncludes(this.pArray, Number(this.index), Number(this.iNum)));
+            },
+            doIncludesB() {
+                Methods.fAlert(Methods.gIncludesB(this.pArray, Number(this.index), Number(this.iNum)));
+            },
+            doSort() {
+                Methods.fAlert(Methods.gSort(this.pArray, Number(this.index)));
+            },
+            doSort2() {
+                Methods.fAlert(Methods.gSort2(this.pArray, Number(this.index)));
+            },
+            doSortB() {
+                Methods.fAlert(Methods.gSortB(this.pArray, Number(this.index)));
+            },
+            doSortQ() {
+                Methods.fAlert(Methods.gSortQ(this.pArray, Number(this.index)));
+            }
         }
-      },
-      doMin() {
-        Methods.fAlert(Methods.gMin(this.pArray));
-      },
-      doMax() {
-        Methods.fAlert(Methods.gMax(this.pArray));
-      },
-      doSum() {
-        Methods.fAlert(Methods.gSum(this.pArray));
-      },
-      doAvg() {
-        Methods.fAlert(Methods.gAvg(this.pArray));
-      },
-      doIncludes() {
-        Methods.fAlert(Methods.gIncludes(this.pArray, Number(this.iNum)));
-      },
-      doIncludesB() {
-        Methods.fAlert(Methods.gIncludesB(this.pArray, Number(this.iNum)));
-      },
-      doSort() {
-        Methods.fAlert(Methods.gSort(this.pArray));
-      },
-      doSort2() {
-        Methods.fAlert(Methods.gSort2(this.pArray));
-      },
-      doSortB() {
-        Methods.fAlert(Methods.gSortB(this.pArray));
-      },
-      doSortQ() {
-        Methods.fAlert(Methods.gSortQ(this.pArray));
-      }
-    }
-  });
+    });
 });
--- app/components/js/util.js
+++ app/components/js/util.js
@@ -1,47 +1,66 @@
 class Methods {
-    
-    static fAlert = _msg => {
-        swal(_msg + "");
-    }
+  static fAlert = _msg => swal(_msg + "");
 
-    static gMin = _array => {
-      return Math.min(..._array);
-    };
-  
-    static gMax = _array => {
-      return Math.max(..._array);
-    };
-  
-    static gSum = _array => {
-      return _array.reduce((a, b) => a + b, 0);
-    };
-  
-    static gAvg = _array => {
-      return this.gSum(_array) / _array.length;
-    };
-  
-    static gIncludes = (_array, _num) => {
-      return _array.includes(_num);
-    };
-  
-    static gIncludesB = (_array, _num) => {
-      return _array.includes(_num);
-    };
-  
-    static gSort = _array => {
-      return _array.sort();
-    };
-  
-    static gSort2 = _array => {
-      return _array.sort();
-    };
-  
-    static gSortB = _array => {
-      return _array.sort();
-    };
-  
-    static gSortQ = _array => {
-      return _array.sort();
-    };
+  static gMin = (_array, _indx) => {
+    let min = _array[0];
+    for (let i = 1; i < _indx; i++) if (_array[i] < min) min = _array[i];
+    return min;
+  };
+
+  static gMax = (_array, _indx) => {
+    let max = _array[0];
+    for (let i = 1; i < _indx; i++) if (_array[i] > max) max = _array[i];
+    return max;
+  };
+
+  static gSum = (_array, _indx) => {
+    let sum = 0;
+    for (let i = 0; i < _indx; i++) sum += _array[i];
+    return sum;
+  };
+
+  static gAvg = (_array, _indx) =>
+    Math.round((this.gSum(_array, _indx) / _indx) * 100) / 100;
+
+  static isBinary = (_array, _indx) => {
+    for (let i = 1; i < _indx; i++) if (_array[i] < _array[i - 1]) return false;
+    return true;
+  };
+
+  static gIncludes = (_array, _indx, _num) => {
+    for (let i = 0; i < _indx; i++) if (_array[i] == _num) return i;
+    return false;
+  };
+
+  static recursiveBinarySearch(_array, _start, _end, _search) {
+    if (_end > _start) {
+      let mid = Math.floor((_start + _end) / 2);
+      if (_array[mid] == _search) return mid;
+      else if (_array[mid] > _search)
+        return this.recursiveBinarySearch(_array, _start, mid - 1, _search);
+      else return this.recursiveBinarySearch(_array, mid + 1, _end, _search);
+    } else return _array[_start] == _search ? _start : -1;
   }
-  
+
+  static gIncludesB = (_array, _indx, _num) => {
+    if (this.isBinary(_array, _indx)) {
+      return this.recursiveBinarySearch(_array, 0, _indx - 1, _num);
+    } else return "List is not binary!";
+  };
+
+  static gSort = (_array, _indx) => {
+    return _array.sort();
+  };
+
+  static gSort2 = (_array, _indx) => {
+    return _array.sort();
+  };
+
+  static gSortB = (_array, _indx) => {
+    return _array.sort();
+  };
+
+  static gSortQ = (_array, _indx) => {
+
+  };
+}
