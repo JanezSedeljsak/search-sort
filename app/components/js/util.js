4c1c6564010a1a778fdec896bd5e44c7fa484653
JanezTheProgramer <43420276+JanezTheProgramer@users.noreply.github.com>
2020-01-06 18:35:59 +0100

    updates

--- app/components/js/util.js
+++ app/components/js/util.js
@@ -1,25 +1,47 @@
-export default class {
-    static findMin = _array => {
-        return Math.max(..._array);
+class Methods {
+    
+    static fAlert = _msg => {
+        swal(_msg + "");
     }
 
-    static findMax = _array => {
-        return Math.min(...array);
-    }
-
+    static gMin = _array => {
+      return Math.min(..._array);
+    };
+  
+    static gMax = _array => {
+      return Math.max(..._array);
+    };
+  
     static gSum = _array => {
-
-    }
-
+      return _array.reduce((a, b) => a + b, 0);
+    };
+  
     static gAvg = _array => {
-
-    }
-
-    static dIncludes = _array => {
-
-    }
-
-    static bIncludes = _array => {
-
-    }
-}
+      return this.gSum(_array) / _array.length;
+    };
+  
+    static gIncludes = (_array, _num) => {
+      return _array.includes(_num);
+    };
+  
+    static gIncludesB = (_array, _num) => {
+      return _array.includes(_num);
+    };
+  
+    static gSort = _array => {
+      return _array.sort();
+    };
+  
+    static gSort2 = _array => {
+      return _array.sort();
+    };
+  
+    static gSortB = _array => {
+      return _array.sort();
+    };
+  
+    static gSortQ = _array => {
+      return _array.sort();
+    };
+  }
+  
